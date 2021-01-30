8622c90f3d760b9b6fe61228e5ceedf46e650403
Maksim Peshkov <peshkov.m@gmail.com>
2021-01-30 21:49:32 +0300

    1

--- test.lua
+++ test.lua
@@ -3,7 +3,31 @@ local term = require('term')
 local radar = component.radar
 local gpu = component.gpu
 
-local players = radar.getPlayers()
+color = {
+	green = 0x00ff00,
+	yellow = 0xffb600,
+	red = 0xff0000
+}
 
-term.clear()
+-- Белый список ---
+white = {
+	Max = true,
+	AdskiyGrifer12 = false
+}
 
+
+gpu.setResolution(35,15)
+
+while true do
+    local players = radar.getPlayers()
+	term.clear()
+	gpu.setForeground(color.yellow)
+	gpu.set(2,1, 'В зоне обнаружения радара: '..#players..' чел.')
+		for i = 1, #players do
+			if white[players[i].name] then gpu.setForeground(color.green)
+			else gpu.setForeground(color.red) end
+			gpu.set(2, i+1, i..'. '..players[i].name)
+			gpu.set(25, i+1, ' - '..floor(players[i].distance)..' м')
+		end
+	os.sleep(30)
+end
