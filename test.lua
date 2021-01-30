857917e96b95d56b95d573974db72f6095961e1e
Maksim Peshkov <peshkov.m@gmail.com>
2021-01-30 21:42:57 +0300

    1

--- test.lua
+++ test.lua
@@ -2,5 +2,5 @@ local component = require('component')
 local radar = component.radar
 local gpu = component.gpu
 
-gpu.setResolution(150,25)
+gpu.setResolution(80,25)
 --local players = radar.getPlayers()
